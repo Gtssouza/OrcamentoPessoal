6901c83487459a5aae56b9486c74222b1a4b23db
Gustavo Souza <gtssouza94@gmail.com>
2020-09-26 15:44:39 -0300

    Implementando filter

--- app.js
+++ app.js
@@ -60,7 +60,36 @@ class Bd {
 	}
 
 	pesquisar(despesa){
+		let despesasFiltradas = Array()
+		despesasFiltradas = this.recuperarTodosRegistros()
 
+		//Aplicando os filtros de:
+		
+		//ano
+		if(despesa.ano != '' ){
+			despesasFiltradas = despesasFiltradas.filter(d => d.ano == despesa.ano)
+		}
+		
+		//mes
+		if(despesa.mes != '' ){
+			despesasFiltradas = despesasFiltradas.filter(d => d.mes == despesa.mes)
+		}
+		//dia
+		if(despesa.dia != '' ){
+			despesasFiltradas = despesasFiltradas.filter(d => d.dia == despesa.dia)
+		}
+		//tipo
+		if(despesa.tipo != '' ){
+			despesasFiltradas = despesasFiltradas.filter(d => d.tipo == despesa.tipo)
+		}
+		//descricao
+		if(despesa.descricao != '' ){
+			despesasFiltradas = despesasFiltradas.filter(d => d.descricao == despesa.descricao)
+		}
+		//valor
+		if(despesa.descricao != '' ){
+			despesasFiltradas = despesasFiltradas.filter(d => d.descricao == despesa.descricao)
+		}
 	}
 }
 
